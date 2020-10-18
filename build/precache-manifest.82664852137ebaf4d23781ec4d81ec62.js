f1695a35aaa7fe8688e82a59bbb5fa4667958358
jcuello123 <60766305+jcuello123@users.noreply.github.com>
2020-10-18 03:53:44 -0400

    Add files via upload

---
+++ build/precache-manifest.82664852137ebaf4d23781ec4d81ec62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e695f1de7ecab073794e388508a33b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8cd921ef8cad7c7b7c0",
+    "url": "/static/css/main.5429eee1.chunk.css"
+  },
+  {
+    "revision": "5be07bc43e8c975ed1b3",
+    "url": "/static/js/2.64b82e87.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.64b82e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cd921ef8cad7c7b7c0",
+    "url": "/static/js/main.b2b468fb.chunk.js"
+  },
+  {
+    "revision": "5e0950e06351c0e83ac7",
+    "url": "/static/js/runtime-main.11c045e8.js"
+  }
+]);
